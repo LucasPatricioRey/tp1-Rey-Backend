98b882151cca9f5e04cdaa2446416c65c346902a
LucasPatricioRey <lucas1998z@hotmail.com>
2023-05-02 14:42:40 -0300

    corrigiendo errores de entrega

--- tp1.js
+++ tp1.js
@@ -1,37 +1,69 @@
-class ProductManager{
-    #products
-    static idCounter
-    constructor(title, description, price, thumbnail, code, stock){
-        this.#products =[]
-        this.idCounter = 0
-        this.id -  ++ this.idCounter
-        this.tittle =title
-        this.description =description
-        this.price =price
-        this.thumbnail =thumbnail
-        this.code =code
-        this.stock =stock
+class ProductManager {
+    #products;
+    static idCounter = 0;
 
+    constructor(title, description, price, thumbnail, code, stock) {
+        //Inicializo la lista de productos y el contador de IDs
+        this.#products = [];
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.thumbnail = thumbnail;
+        this.code = code;
+        this.stock = stock;
     }
 
-addProduct(tittle, description, price, thumbnail, code, stock ){
-    let product = {
-        tittle,
-        description,
-        price,
-        code,
-        thumbnail,
-        stock,
-        id: this.#getId(),
-    };
-    this.#products.push(newProduct)
-    return console.log("produto añadido")    
+    // Agrego un producto a la lista de productos
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if (title == undefined || description == undefined || price == undefined || thumbnail == undefined || code == undefined || stock == undefined)
+            return console.log("Todos los campos son obligatorios");
 
-}getProducts(){
-    return console.log(this.#products)
-}
-getProductById(){
-    
+        if (this.#products.find(p => p.code === code))
+            return console.log("Ya existe un producto con este codigo");
+
+        const newProduct = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+            id: ++ProductManager.idCounter
+        };
+
+        this.#products.push(newProduct);
+
+        return console.log("Producto Añadido");
+    }
+
+    // Devuelve la lista completa de productos
+    getProducts() {
+        return console.log(this.#products);
+    }
+
+    // Devuelve el producto que tiene el ID especificado
+    getProductById(pid) {
+        if (this.#products.find(p => p.id === pid)) {
+            const productFound = this.#products.find(p => p.id === pid);
+            return console.log(productFound);
+        }
+        return console.log("No existe un producto con ese id");
+    }
 }
 
-}
+const productManager = new ProductManager();
+
+productManager.addProduct("Mancuerna", "Articulo de gimnasia", "2500", "/images/gimansio.png", "2", 5);
+productManager.addProduct("Colchoneta de gimnasia", "Articulo de gimnasia", "1500", "/images/gimansio.png", "2", 5);
+productManager.addProduct("Pesa rusa", "Articulo de gimnasia", "3500", "/images/gimansio.png", "4", 5);
+productManager.addProduct("Barra de gimnasio", "Articulo de gimnasia", "3000", "/images/gimansio.png", "5", 5);
+productManager.addProduct("Disco de pesa", "Articulo de gimnasia", "7620", "/images/gimansio.png", "6", 5);
+productManager.addProduct("Banco inclinable", "Articulo de gimnasia", "4000", "/images/gimansio.png", "7", 5);
+productManager.addProduct("Guantes deportivos", "Articulo de gimnasia", "700", "/images/gimansio.png", "8", 5);
+productManager.addProduct("Conjunto de ropa deportiva", "Articulo de gimnasia", "10000", "/images/gimansio.png", "9", 5);
+
+// Muestra la lista completa de productos
+// productManager.getProducts();
+
+// Busca y muestra el producto con ID 1
+// productManager.getProductById(1);
